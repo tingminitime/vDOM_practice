4574463f87907bca92b919852418b421fd130666
tingminitime <e0610k@gmail.com>
2021-12-10 18:12:02 +0800

    thinking...

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # vDOM_practice
+
+## Reference
+[Virtual DOM | 為了瞭解原理，那就來實作一個簡易 Virtual DOM 吧！](https://medium.com/%E6%89%8B%E5%AF%AB%E7%AD%86%E8%A8%98/build-a-simple-virtual-dom-5cf12ccf379f)
--- diff.js
+++ diff.js
@@ -1,7 +1,9 @@
 import mount from "./mount"
 import render from "./render"
 
+// 這邊都是回傳函式
 const diff = function (vOldNode, vNewNode) {
+  // 傳進來的新節點如果是 undefined，代表該節點是要被刪除的節點，return undefined
   if (vNewNode === undefined) {
     return $node => {
       $node.remove()
@@ -9,17 +11,21 @@ const diff = function (vOldNode, vNewNode) {
     }
   }
 
+  // 如果傳進來的節點是字串
   if (typeof vOldNode === 'string' || typeof vNewNode === 'string') {
+    // 如果字串有改變，就回傳掛載函式 (舊字串 => 新字串)
     if (vOldNode !== vNewNode) {
       return $node => {
         const $newNode = render(vNewNode)
         return mount($newNode, $node)
       }
+      // 如果字串沒有改變，就回傳一樣的節點
     } else {
       return $node => $node
     }
   }
 
+  // 如果新舊節點標籤不一樣，則新節點取代舊節點
   if (vOldNode.tagName !== vNewNode.tagName) {
     return $node => {
       const $newNode = render(vNewNode)
@@ -27,8 +33,9 @@ const diff = function (vOldNode, vNewNode) {
     }
   }
 
+  // 比較新舊節點的屬性，
   const patchAttrs = diffAttrs(vOldNode.attrs, vNewNode.attrs)
-  const patchChildren = diffChildren(vOldNode.chilldren, vNewNode.children)
+  const patchChildren = diffChildren(vOldNode.children, vNewNode.children)
 
   return $node => {
     patchAttrs($node)
@@ -37,10 +44,12 @@ const diff = function (vOldNode, vNewNode) {
   }
 }
 
+// 這邊也都是回傳函式
 const diffAttrs = function (oldAttrs, newAttrs) {
+  // 儲存屬性變動的 patch 函式
   const patches = []
 
-  // 新增新屬性到 patches 陣列
+  // 新增 "設定新屬性的函式" 到 patches 陣列
   for (const key in newAttrs) {
     patches.push($node => {
       $node.setAttribute(key, newAttrs[key])
@@ -48,7 +57,7 @@ const diffAttrs = function (oldAttrs, newAttrs) {
     })
   }
 
-  // 如果"沒有"舊屬性在新屬性上，
+  // 如果 "沒有" 舊屬性在新屬性上，就新增 "移除舊屬性函式" 到 patches 陣列
   for (const key in oldAttrs) {
     if (!(key in newAttrs)) {
       patches.push($node => {
@@ -58,11 +67,47 @@ const diffAttrs = function (oldAttrs, newAttrs) {
     }
   }
 
+  // 回傳更新函式，這個函式會執行在 patches 陣列裡每個函式
   return $node => {
     patches.forEach(patch => {
       patch($node)
     })
   }
+}
+
+const diffChildren = function (oldVChildren, newVChildren) {
+  //
+  const childPatches = []
+  oldVChildren.forEach((oldVChild, i) => {
+    childPatches.push(diff(oldVChild, newVChildren[i]))
+  })
+
+  //
+  const additionalPatches = []
+  newVChildren.slice(oldVChildren.length).forEach(additionalVChild => {
+    additionalPatches.push($node => {
+      $node.appendChild(render(additionalVChild))
+      return $node
+    })
+  })
+
+  //
+  return $node => {
+    //
+    for (let i = childPatches.length - 1; i >= 0; i--) {
+      const $child = $node.childNodes[i]
+      const patch = childPatches[i]
+      patch($child)
+    }
+
+    //
+    additionalPatches.forEach(patch => {
+      patch($node)
+    })
+
+    return $node
+  }
+
 }
 
 export default diff
