7573957fff6d673c804b3a21aab9295051736a0b
tingminitime <e0610k@gmail.com>
2021-12-14 13:45:50 +0800

    update proxy

--- vNode.js
+++ vNode.js
@@ -3,19 +3,23 @@ import render from "./render.js"
 import mount from "./mount.js"
 import diff from "./diff.js"
 
-const createVApp = function (count) {
+// Proxy
+const createVApp = function ({ count }) {
   return createElement("div", {
     attrs: {
       id: "app",
       dataCount: count,
     },
     children: [
-      createElement("input"),
+      createElement("button", {
+        attrs: { id: "btn" },
+        children: [String('Get a random number')]
+      }),
       String(`Current count: ${count}`),
       ...Array.from({ length: count }, () =>
         createElement("img", {
           attrs: {
-            src: "https://picsum.photos/200/300?random=1"
+            src: "https://picsum.photos/200/200?random=1"
           }
         })
       )
@@ -23,15 +27,66 @@ const createVApp = function (count) {
   })
 }
 
-let count = 0
-let vApp = createVApp(count)
-const $app = render(vApp)
-let $rootEl = mount($app, document.getElementById("app"))
+const handler = {
+  set: (obj, prop, value) => {
+    obj[prop] = value
 
-setInterval(() => {
-  count = Math.floor(Math.random() * 10)
-  const vNewApp = createVApp(count)
-  const patch = diff(vApp, vNewApp)
-  $rootEl = patch($rootEl)
-  vApp = vNewApp
-}, 1000)
+    const vNewApp = createVApp(obj)
+    const patch = diff(vApp, vNewApp)
+    $rootEl = patch($rootEl)
+    vApp = vNewApp
+
+    return true
+  }
+}
+
+let data = { count: 0 }
+const proxyData = new Proxy(data, handler)
+console.log(proxyData)
+
+// init
+let vApp = createVApp(proxyData)
+const $app = render(vApp)
+let $rootEl = mount($app, document.getElementById('app'))
+
+// Listener
+const button = document.getElementById('btn')
+button.addEventListener('click', () => {
+  const newCount = Math.floor(Math.random() * 10)
+  proxyData.count = newCount
+})
+
+
+// Timer
+// const createVApp = function (count) {
+//   return createElement("div", {
+//     attrs: {
+//       id: "app",
+//       dataCount: count,
+//     },
+//     children: [
+//       createElement("input"),
+//       String(`Current count: ${count}`),
+//       ...Array.from({ length: count }, () =>
+//         createElement("img", {
+//           attrs: {
+//             src: "https://picsum.photos/200/300?random=1"
+//           }
+//         })
+//       )
+//     ]
+//   })
+// }
+
+// let count = 0
+// let vApp = createVApp(count)
+// const $app = render(vApp)
+// let $rootEl = mount($app, document.getElementById("app"))
+
+// setInterval(() => {
+//   count = Math.floor(Math.random() * 10)
+//   const vNewApp = createVApp(count)
+//   const patch = diff(vApp, vNewApp)
+//   $rootEl = patch($rootEl)
+//   vApp = vNewApp
+// }, 1000)
