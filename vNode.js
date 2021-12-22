4335c2fa2be76487f5b21a6e5ecc8f7aa31d991e
tingminitime <e0610k@gmail.com>
2021-12-22 17:25:11 +0800

    更改DOM

--- vNode.js
+++ vNode.js
@@ -3,6 +3,20 @@ import render from "./render.js"
 import mount from "./mount.js"
 import diff from "./diff.js"
 
+// template
+const appDom = /* html */ `
+<div id="app">
+  <div class="container" style="display: flex; flex-direction: column; align-items: center;">
+    <button id="btn">Get a random number</button>
+    <div>Current count: {{ count }}</div>
+    <div class="imgContainer">
+      <!-- 隨機產生 0~10 個 image -->
+      <img src="./cat.jpg" alt="cat meme" style="width: 100px;">
+    </div>
+  </div>
+</div>
+`
+
 // Proxy
 const createVApp = function ({ count }) {
   return createElement("div", {
@@ -11,18 +25,39 @@ const createVApp = function ({ count }) {
       dataCount: count,
     },
     children: [
-      createElement("button", {
-        attrs: { id: "btn" },
-        children: [String('Get a random number')]
+      createElement("div", {
+        attrs: {
+          class: "container",
+          style: "display: flex; flex-direction: column; align-items: center;"
+        },
+        children: [
+          createElement("button", {
+            attrs: { id: "btn" },
+            children: [String('Get a random number')]
+          }),
+          createElement("div", {
+            children: [
+              String(`Current count: ${count}`),
+            ]
+          }),
+          createElement("div", {
+            attrs: {
+              class: "imgContainer"
+            },
+            children: [
+              ...Array.from({ length: count }, () =>
+                createElement("img", {
+                  attrs: {
+                    src: "./cat.jpg",
+                    alt: "cat meme",
+                    style: "width: 100px; margin: 4px 4px;"
+                  }
+                })
+              )
+            ]
+          }),
+        ]
       }),
-      String(`Current count: ${count}`),
-      ...Array.from({ length: count }, () =>
-        createElement("img", {
-          attrs: {
-            src: "https://picsum.photos/200/200?random=1"
-          }
-        })
-      )
     ]
   })
 }
